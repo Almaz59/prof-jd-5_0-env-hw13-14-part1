3a8060a3807dacb6004e9ef12c2ed57aba75374e
Almaz59 <100568607+Almaz59@users.noreply.github.com>
2022-06-12 15:09:24 +0500

    hw1314part1 обязательные задачи

---
+++ src/test/java/pro/sky/profjd5_0envhw1314part1/CalculatorServiceTestConstants.java
@@ -0,0 +1,9 @@
+package pro.sky.profjd5_0envhw1314part1;
+
+public class CalculatorServiceTestConstants {
+    public static final int CORRECT_NUMBER_1 = 2;
+    public static final int CORRECT_NUMBER_2 = 5;
+    public static final int CORRECT_NUMBER_3 = 23;
+    public static final int CORRECT_NUMBER_4 = 26;
+    public static final int CORRECT_NUMBER_5 = 0;
+}
